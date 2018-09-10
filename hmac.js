b9fc38ffce3bc93c0f95a92ec8f95057326ffdcc
hendratay <hendratay96@gmail.com>
2018-09-10 09:28:39 +0700

    generate serial number with `voucher code generator`

--- hmac.js
+++ hmac.js
@@ -1,14 +1,18 @@
-const CryptoJS = require('crypto-js')
+const voucher_codes = require('voucher-code-generator');
 const crypto = require('crypto')
+const CryptoJS = require('crypto-js')
+
+// voucherify random code generator
+let serialNumber = voucher_codes.generate({ length: 20, count: 1, charset: voucher_codes.charset("alphanumeric") })[0];
+// csprng: cryptographically secure pseudo random number generator
+let randomKey = crypto.randomBytes(16).toString('base64');
 
 // encrypt
-let serialNumber = "qwerasdfzxcv1234" // voucherify random code generator
-let randomKey = crypto.randomBytes(16).toString('base64'); // csprng: cryptographically secure pseudo random number generator
 let hash = CryptoJS.HmacSHA256(serialNumber, randomKey); // output WordArray
 let hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
 console.log("encrypt: ", hashInBase64);
 
 // decrypt
-let parsedWordArray = CryptoJS.enc.Base64.parse(hashInBase64); // output WordArray
-let parsedHash = parsedWordArray.toString(CryptoJS.enc.Base64);
-console.log("decrypt: ", parsedHash);
+let parsedHash = CryptoJS.enc.Base64.parse(hashInBase64); // output WordArray
+let parsedHashInBase64 = parsedHash.toString(CryptoJS.enc.Base64);
+console.log("decrypt: ", parsedHashInBase64);
